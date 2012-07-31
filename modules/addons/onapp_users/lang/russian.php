5a345f15f3926c2e713845a40662b0b2c9ecae61
Denis <denis@tuthost.com>
2012-07-31 10:46:29 +0300

    Продолжил перевод

--- modules/addons/onapp_users/lang/russian.php
+++ modules/addons/onapp_users/lang/russian.php
@@ -31,35 +31,35 @@ $_ADDONLANG[ 'MappedFilter' ] = 'показать только mapped польз
 
 // stat
 $_ADDONLANG[ 'TotalAmount' ] = 'Общая сумма';
-$_ADDONLANG[ 'PaymentAmount' ] = 'Payment Amount';
-$_ADDONLANG[ 'OutstandingAmount' ] = 'Outstanding Amount';
+$_ADDONLANG[ 'PaymentAmount' ] = 'Сумма платежа';
+$_ADDONLANG[ 'OutstandingAmount' ] = 'Непогашенная сумма';
 $_ADDONLANG[ 'MemoryAvailable' ] = 'Доступно памяти';
 $_ADDONLANG[ 'UsedMemory' ] = 'Использовано памяти';
-$_ADDONLANG[ 'DiskSpaceAvailable' ] = 'Disk Space Available';
+$_ADDONLANG[ 'DiskSpaceAvailable' ] = 'Доступное дисковое пространство';
 $_ADDONLANG[ 'UsedDiskSize' ] = 'Использовано дискового пространства';
 $_ADDONLANG[ 'UsedCPUs' ] = 'Использовано CPUs';
-$_ADDONLANG[ 'UsedCPUShares' ] = 'Used CPU Shares';
+$_ADDONLANG[ 'UsedCPUShares' ] = 'Использовано CPU Shares';
 $_ADDONLANG[ 'Activate' ] = 'Активировать';
 $_ADDONLANG[ 'Suspend' ] = 'Заблокировать';
 $_ADDONLANG[ 'Unmap' ] = 'Unmap';
 $_ADDONLANG[ 'LoginToCP' ] = 'Войти в КП';
 
 // messages
-$_ADDONLANG[ 'ActivatedSuccessfully' ] = 'User was activated successfully';
-$_ADDONLANG[ 'ActivatedError' ] = 'There was error during user activation: ';
+$_ADDONLANG[ 'ActivatedSuccessfully' ] = 'Пользователь успешно активирован';
+$_ADDONLANG[ 'ActivatedError' ] = 'Произошла ошибка при активации пользователя: ';
 $_ADDONLANG[ 'MapedSuccessfully' ] = 'User was mapped successfully';
 $_ADDONLANG[ 'MapedError' ] = 'There was error during user\'s maping: ';
 $_ADDONLANG[ 'MapedErrorExists' ] = 'user was already mapped';
 $_ADDONLANG[ 'UnmapedSuccessfully' ] = 'User was unmapped successfully';
 $_ADDONLANG[ 'UnmapedError' ] = 'There was error during user\'s unmaping: ';
-$_ADDONLANG[ 'DataSyncedSuccessfully' ] = 'Data was synced successfully';
-$_ADDONLANG[ 'DataSyncedError' ] = 'There was error during data synchronization: ';
-$_ADDONLANG[ 'SuspendSuccessfully' ] = 'User was suspended successfully';
-$_ADDONLANG[ 'SuspendError' ] = 'There was error during user suspending: ';
+$_ADDONLANG[ 'DataSyncedSuccessfully' ] = 'Данные успешно синхронизированы';
+$_ADDONLANG[ 'DataSyncedError' ] = 'Произошла ошибка при синхронизации данных: ';
+$_ADDONLANG[ 'SuspendSuccessfully' ] = 'Пользователь успешно заблокирован';
+$_ADDONLANG[ 'SuspendError' ] = 'Произошла ошибка при блокировании пользователя: ';
 $_ADDONLANG[ 'AuthSyncedSuccessfully' ] = 'Login and Password are now identical';
 $_ADDONLANG[ 'AuthSyncedError' ] = 'There was error during authentication synchronization: ';
-$_ADDONLANG[ 'BlockOpsSuccessfully' ] = 'Operations were performed successfully';
-$_ADDONLANG[ 'BlockOpsError' ] = 'There were some errors during performing operations';
+$_ADDONLANG[ 'BlockOpsSuccessfully' ] = 'Операция успешно выполнена';
+$_ADDONLANG[ 'BlockOpsError' ] = 'Произошли ошибки при выполнении операций';
 $_ADDONLANG[ 'ID' ] = 'ID';
 
 // JavaScript
